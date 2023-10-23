d70af4c140cc497a9c010cd022ea1a5fcbb0b13d
Beloded1 <alexandrbeloded@gmail.com>
2023-10-23 15:32:10 +0400

    Done ex.1.4

--- pcost.py
+++ pcost.py
@@ -1,10 +1,32 @@
-total_cost = 0.0
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename) as f:
+        for line in f:
+            fields = line.split()
+            nshares = int(fields[1])
+            price = float(fields[2])
+            total_cost = total_cost + nshares * price
+    return total_cost
 
-with open('/home/alexandr/projects/python-mastery/Data/portfolio.dat', 'r') as f:
-    for line in f:
-        fields = line.split()
-        nshares = int(fields[1])
-        price = float(fields[2])
-        total_cost = total_cost + nshares * price
 
-print(total_cost)
+print(portfolio_cost('Data/portfolio.dat'))
+
+
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename) as f:
+        for line in f:
+            fields = line.split()
+            try:
+                nshares = int(fields[1])
+                price = float(fields[2])
+                total_cost = total_cost + nshares*price
+
+            # This catches errors in int() and float() conversions above
+            except ValueError as e:
+                print("Couldn't parse:", repr(line))
+                print("Reason:", e)
+    return total_cost
+
+
+print(portfolio_cost('Data/portfolio3.dat'))
