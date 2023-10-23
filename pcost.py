9687d9191a46c503e499a05e5b4233e71a0b2a5c
Beloded1 <alexandrbeloded@gmail.com>
2023-10-23 15:20:39 +0400

    Done ex.1-3

---
+++ pcost.py
@@ -0,0 +1,10 @@
+total_cost = 0.0
+
+with open('/home/alexandr/projects/python-mastery/Data/portfolio.dat', 'r') as f:
+    for line in f:
+        fields = line.split()
+        nshares = int(fields[1])
+        price = float(fields[2])
+        total_cost = total_cost + nshares * price
+
+print(total_cost)
