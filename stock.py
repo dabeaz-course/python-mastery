11d67fcd1a5125bec84a0b69475b20fe2c71a666
Beloded1 <alexandrbeloded@gmail.com>
2023-10-25 15:57:38 +0400

    Done Ex. 1_5

---
+++ stock.py
@@ -0,0 +1,8 @@
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
