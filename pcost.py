0551ba25314a8b61ca4377893e283ed2a7188d7c
Beloded1 <alexandrbeloded@gmail.com>
2023-11-09 16:09:07 +0400

    Add requirements.txt and done some work

---
+++ pcost.py
@@ -0,0 +1,18 @@
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename) as f:
+        for line in f:
+            fields = line.split()
+            try:
+                nshares    = int(fields[1])
+                price      = float(fields[2])
+                total_cost = total_cost + nshares*price
+
+            # This catches errors in int() and float() conversions above
+            except ValueError as e:
+                print("Couldn't parse:", repr(line))
+                print("Reason:", e)
+
+    return total_cost
+
+print(portfolio_cost('/home/alexandr/projects/python-mastery_new/Data/portfolio.dat'))
