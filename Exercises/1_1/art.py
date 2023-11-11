d8a62115e12953e3f255e32446dee9eb6c1b03a4
Thiago Assis <thiago.medassis@gmail.com>
2023-11-11 17:49:07 -0300

    Solved 1_1

---
+++ Exercises/1_1/art.py
@@ -0,0 +1,15 @@
+import random
+import sys
+
+chars = "\\|/"
+
+
+def draw(rows, columns):
+    for _ in range(rows):
+        print("".join(random.choice(chars) for _ in range(columns)))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        raise SystemExit("Usage: art.py rows columns")
+    draw(int(sys.argv[1]), int(sys.argv[2]))
