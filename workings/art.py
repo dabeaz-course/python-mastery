16a720eb97cac8406e6c535a181c04c37d1b6057
gasaroleila <uwamgaleila@gmail.com>
2024-06-04 18:08:17 +0200

    worked up to page:100 next start: Exos 2.2

---
+++ workings/art.py
@@ -0,0 +1,13 @@
+import sys
+import random
+
+chars = '\|/'
+
+def draw(rows, columns):
+    for r in range(rows):
+        print(''.join(random.choice(chars) for _ in range(columns)))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        raise SystemExit("Usage: art.py rows columns")
+    draw(int(sys.argv[1]), int(sys.argv[2]))
---
+++ workings/readrides.py
@@ -0,0 +1,122 @@
+import csv
+import typing
+import tracemalloc
+
+
+
+def read_rides_as_tuples(filename):
+    '''
+    Read the bus ride data as a list of tuples
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = (route, date, daytype, rides)
+            records.append(record)
+    return records
+
+def read_rides_as_dictionary(filename):
+    '''
+    Read the bus ride data as a list of dictionaries
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = {
+                'route': route,
+                'date': date,
+                'dayType': daytype,
+                'rides': rides
+             }
+            records.append(record)
+    return records
+
+class Row:
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+def read_rides_as_class(filename):
+    '''
+    Read the bus ride data as a list of dictionaries
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = Row(route,date,daytype,rides)
+            records.append(record)
+    return records
+
+class Stock(typing.NamedTuple):
+ route: str
+ date: str
+ daytype: str
+ price: int
+
+def read_rides_as_named_tuple(filename):
+    '''
+    Read the bus ride data as a list of dictionaries
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = Stock(route,date,daytype,rides)
+            records.append(record)
+    return records
+
+class NewRow:
+    __slots__ = ['route', 'date', 'daytype', 'rides']
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+def read_rides_as_class_slots(filename):
+    '''
+    Read the bus ride data as a list of dictionaries
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)     # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = NewRow(route,date,daytype,rides)
+            records.append(record)
+    return records
+
+# ranking: class with slots, tuples, named tuples, Class, dictionaries
+if __name__ == '__main__':
+    tracemalloc.start()
+    rows = read_rides_as_class_slots('../Data/ctabus.csv')
+    print('Memory Use: Current %d, Peak %d' % tracemalloc.get_traced_memory())
